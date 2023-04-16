b1ce4b1766f8d355b282c068af79436c172b86b2
halac123b <halac123b@gmail.com>
2023-04-16 13:32:51 +0700

    Final upload

---
+++ GameDev-Ass4-BoardGame-main/Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using GameMode = Mode.GameMode;
+
+public class MenuController : MonoBehaviour
+{
+    public void PlayPvP() {
+        Mode.mode = GameMode.PvP;
+        SceneManager.LoadScene("Game");
+    }
+
+    public void PlayAI() {
+        Mode.mode = GameMode.AI;
+        SceneManager.LoadScene("Game");
+    }
+
+    public void PlayAIHard() {
+        Mode.mode = GameMode.AIHard;
+        SceneManager.LoadScene("Game");
+    }
+
+    public void PlayOnline()
+  {
+    Mode.mode = GameMode.PvP;
+    SceneManager.LoadScene("Multi");
+  }
+
+    public void ExitGame()
+    {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif    
+    }
+
+}
